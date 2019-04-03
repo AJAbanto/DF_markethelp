1c459eb0be120f4d1d9256330b5205ebafe47376
AJAbanto <abantoalfred@yahoo.com>
2019-04-04 03:10:28 +0800

    Changes to be committed: 	modified:   df_market.py 	new file:   df_mp2_logger.json 	new file:   df_mp_logger.json
    Update Notes:
    	-Added interfacing with Google sheets
    	-Logging with google sheets is now also possible
    
    To do:
    	-Periodic logging feature
    	-email alert
    	-compile to run on mobile server

--- df_market.py
+++ df_market.py
@@ -1,4 +1,8 @@
 import re
+import gspread
+import datetime
+
+from oauth2client.service_account import ServiceAccountCredentials
 from robobrowser import RoboBrowser
 
 
@@ -42,7 +46,37 @@ ave = 0
 for i in range(5):
 	ave +=int(clean_result[i].replace(',',''))
 ave *= 1/5
-ave = int(ave)
-print(ave)
+ave = int(ave) #This is average price of the 5 cheapest items on MP
+
+
+#Logging data into google sheets
+print('Appending Info to Google sheets\n')
+
+#accessing APIs
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('df_mp2_logger.json',scope)
+
+#authorizing
+client = gspread.authorize(creds) 
+
+#accessing sheets
+sheet = client.open('Deadfrontier_Market_analysis').worksheet('12.7mm Ammo')
+
+#preparing data (Time stamp)
+time_stamp = datetime.datetime.now() #timestamp
+date = time_stamp.strftime('%m/%d/%Y')	#formatting timestamp
+time = time_stamp.strftime('%I:%M %p')
+Lbound_price = ave	#lowerbound price average
+
+new_row =[date, time ,Lbound_price]
+#print(new_row)
+sheet.insert_row(new_row)
+
+
+
+
+
+
+
 
 
